c8a59ca53c25d5e6449193f3d72c6c8523934122
AgbajiAlexander <alexanderogah@gmail.com>
2026-07-10 13:05:03 +0100

    my first text editor project in python

--- text-tool/test_playground.py
+++ text-tool/test_playground.py
@@ -6,8 +6,19 @@
 # result = " ".join(words)
 # print(result)
 
-from transformations import convert_hex
+from transformations import convert_hex, convert_bin
 
-text = "1E (hex) files were added"
-result = convert_hex(text)
-print(result)
+# text1 = "1E (hex) files were added"
+# result = convert_hex(text1)
+# print(result)
+
+# text2 =  "It has been 10 (bin) years" 
+# result = convert_bin(text2)
+# print(result)
+
+text1 = "1E (hex) files were added"
+print(convert_hex(text1))
+
+text2 = "It has been 10 (bin) years"
+print(convert_bin(text2))
+
--- text-tool/transformations.py
+++ text-tool/transformations.py
@@ -12,3 +12,18 @@ def convert_hex(text):
             result_words.append(words[i])
 
     return " ".join(result_words)
+
+def convert_bin(text):
+    words = text.split()
+    result_words =[]
+
+    for i in range(len(words)):
+        if words[i] == "(bin)":
+            result_words.pop()
+            bin_value = words[i-1]
+            decimal_value = int(bin_value, 2)
+            result_words.append(str(decimal_value))
+        else:
+            result_words.append(words[i])
+
+    return " ".join(result_words)
